35c8e3a4d17b1c6f947252ccb0f3a2f47db1aff0
jeremyLtz <jltzcodes@gmail.com>
2023-07-11 19:53:22 -0600

    SetUp p3

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,21 +1,11 @@
-async function getData() {
-  const res = await fetch("http://localhost:3000/api/applications");
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
-const ExamplePage = async () => {
+const Applications = () => {
   // Render the data on the page
-  const data = await getData();
   return (
     <div>
       <h1>API Response:</h1>
-      <pre>{JSON.stringify(data)}</pre>
+      <pre>Whaddup</pre>
     </div>
   );
 };
 
-export default ExamplePage;
+export default Applications;
