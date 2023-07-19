2ea8bde81da4fc96d2280de149b0d3f9ca18d82a
jeremyLtz <jltzcodes@gmail.com>
2023-07-18 18:13:40 -0600

    Create Landing Page and SignIn routes

--- src/app/api/applications/route.ts
+++ src/app/api/applications/route.ts
@@ -1,3 +1,14 @@
-export async function GET(request: Request) {
-  return new Response("Heyyo!");
+import { PrismaClient } from "@prisma/client";
+import { auth } from "@clerk/nextjs";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const { userId } = auth();
+  if (!userId) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+  const prisma = new PrismaClient();
+  const applications = await prisma.applications.findMany();
+  console.log(applications);
+  return new Response(JSON.stringify(applications));
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,41 @@
 import styles from "./page.module.css";
-import { currentUser, SignInButton } from "@clerk/nextjs";
+import { currentUser, SignInButton, SignOutButton } from "@clerk/nextjs";
 
 export default async function Home() {
   const user = await currentUser();
   return (
     <main className={styles.main}>
-      {user && <span>{`Hey ${user.firstName}`}</span>}
-      {!user && <SignInButton />}
+      <nav className={styles.homeNav}>
+        <span>Artemis Logo</span>
+        {user && (
+          <div className={styles.userContainer}>
+            <img
+              className={styles.profilePic}
+              src={user.profileImageUrl}
+              alt='User Profile Picture'
+            />
+            <a
+              className={styles.dashboardButton}
+              href='/app/dashboard'
+              rel='noopener norefferer'>
+              Dashboard
+            </a>
+            <SignOutButton>
+              <button className={styles.signOut}>Sign Out</button>
+            </SignOutButton>
+          </div>
+        )}
+        {!user && (
+          <SignInButton>
+            <button className={styles.signIn}>Sign In</button>
+          </SignInButton>
+        )}
+      </nav>
+      <header className={styles.homeHeader}>
+        <h1>
+          On the hunt? Track your applications with <span>Artemis</span>
+        </h1>
+      </header>
     </main>
   );
 }
