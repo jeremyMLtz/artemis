fec6770a2da0787e6c54c3400b8522af0fe37293
jeremyLtz <jltzcodes@gmail.com>
2023-07-18 18:29:58 -0600

    ignoring this one too

--- src/app/app/dashboard/page.tsx
+++ src/app/app/dashboard/page.tsx
@@ -4,12 +4,12 @@ import { PrismaClient } from "@prisma/client";
 async function getData() {
   // const data = await fetch("http://0.0.0.0:3000/api/applications");
   // return data.json();
-  const user = await currentUser();
-  const prisma = new PrismaClient();
-  const applications = await prisma.applications.findMany({
-    where: { userId: { equals: user?.id } },
-  });
-  return applications;
+  // const user = await currentUser();
+  // const prisma = new PrismaClient();
+  // const applications = await prisma.applications.findMany({
+  //   where: { userId: { equals: user?.id } },
+  // });
+  return [];
 }
 const Dashboard = async () => {
   const data = await getData();
