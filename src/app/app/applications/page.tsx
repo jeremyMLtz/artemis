d558baa9253a2699e6a7a4f210c3316b6025d38c
jeremyLtz <jltzcodes@gmail.com>
2023-07-18 18:28:29 -0600

    Aight, we'll figure this out on the next branch

--- src/app/app/applications/page.tsx
+++ src/app/app/applications/page.tsx
@@ -2,15 +2,16 @@ import { currentUser } from "@clerk/nextjs";
 import { PrismaClient } from "@prisma/client";
 
 async function getData() {
-  const user = await currentUser();
-  const prisma = new PrismaClient();
-  if (!user) {
-    return;
-  }
-  const applications = await prisma.applications.findMany({
-    where: { userId: user.id },
-  });
-  return applications;
+  // const user = await currentUser();
+  // const prisma = new PrismaClient();
+  // if (!user) {
+  //   return;
+  // }
+  // const applications = await prisma.applications.findMany({
+  //   where: { userId: user.id },
+  // });
+  // return applications;
+  return [];
 }
 const Applications = async () => {
   const data = await getData();
